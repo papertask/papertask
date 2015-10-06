c785463de0cb0280cae7ee89e17d52795df8ce49
dinhthi1610 <todinhthi@gmail.com>
2015-10-06 16:51:31 +0700

    fix 197

--- module/Application/config/module.acl.roles.php
+++ module/Application/config/module.acl.roles.php
@@ -53,7 +53,158 @@ $allow_employer = array(
 		'task\getFreelancerAssigningTaskList',
 		
 	// -- Project
-	'project\index',
+	
+	'project\quote', // Quotes in menu
+	'project\detail', // Quotes in menu
+	'project\uploadFile',
+	'project\quoteprint',
+	'project\quotedownload',
+	'project\invoiceprint',
+	'project\invoicedownload',
+	'project\order-translation',
+	'project\need-quote',
+	'project\order-translation-non-contract',
+	'project\client-projects',
+	'project\client-ongoing-projects',
+	'project\client-review-projects',
+	'project\client-completed-projects',
+	'project\client-quotes',
+	'project\quote-detail',
+	'project\wordcount',
+	'project\uploadtemp',
+	'project\getClientProjectList',
+	
+	// -- Quote	
+	'quote\detail',
+	'quote\new',
+	
+	
+	// -- Freelancer
+	'freelancer\index',
+	'freelancer\download',
+	
+	'freelancer\new',
+	'freelancer\detail',
+	'freelancer\edit-profile',
+	'freelancer\uploadFile',
+	'freelancer\getFreelancesList',
+	'freelancer\edit-payment-info',
+	'freelancer\edit-price',
+	'freelancer\finishRegistration',
+	'freelancer\getuserbyfreelancerid',
+	
+	// -- Employer (Client)
+	'employer\list',
+	'employer\new',
+	'employer\profile',
+	'employer\detail',
+	'employer\edit',
+	'employer\removeResource',
+	'employer\addResource',
+	// -- Staff
+	'staff\index',
+	'staff\new',
+	'staff\getPmList',
+	'staff\getSalesList',
+	'staff\view',
+	'staff\edit-profile',
+	'staff\uploadFile',
+	//finance
+	'finance\client-unpaid',
+		'finance\client-unpaid-project',
+	'finance\freelancer-unpaid',
+	'finance\add-incomming',
+	'finance\add-outgoing',
+	'finance\getProjectUnpaidList',
+	'finance\getTaskUnpaidList',
+	'finance\getClientUnpaid',
+	'finance\getFreelancerUnpaid',
+	'finance\transaction',
+	'finance\report',
+	'finance\getTransactionList',
+	'finance\incomming-detail',
+	'finance\outgoing-detail',
+	'finance\getTransaction',
+	'finance\getTransactionTask',
+	
+	'finance\searchProjectUnpaidList',
+	'finance\advsearchProjectUnpaidList',
+	'finance\freelancer-transaction',
+	'finance\getFreelancerOutTransactionList',
+	'finance\freelancer-unpaid-task',
+	
+	// -- Email
+	'email\index',
+	'email\new',
+	'email\edit',
+	'email\loadTemplate',
+	'email\check',
+	// -- Profile
+	'papertask\profile',
+	// -- API
+	'api/common',
+	'api/user',
+	'api/data',
+	'api/admin',
+	'api/papertask',
+	'api/companyinfo',
+	'api/user_child',
+);
+$allow_freelancer = array(
+	// Static pages
+	'index\index',
+	'index\home',
+	'index\languages',
+	'index\freelancer',
+	'index\contact',
+	
+	'index\contact-post',
+
+	'index\order',
+	'index\privacy',
+	'index\terms',
+	'home',
+	'languages',
+	'freelancer',
+	'contact',
+	'order',
+	'privacy',
+	'terms',
+
+	// Login & Register
+	'login\index',
+	'login\form',
+	'forgotpassword\index',
+	'forgotpassword\reset',
+	'forgotpassword\resetpassword',
+	'login\social',
+	'logout\index',
+	'register\index',
+	'register\confirm',
+	'register\social',
+	'register\employer',
+	'register\freelancer',
+	'resetpassword\index',
+
+	// Dashboard Panel
+	'dashboard\index',
+	'dashboard\client-dashboard',
+	'dashboard\freelancer-dashboard',
+	'dashboard\admin-pm-dashboard',
+	// -- Task
+	'task\index',
+	'task\detail',
+	'task\freelancer-task-view',
+	'task\getFreelancerTaskList',
+	'task\FreelancerAcceptTask',
+	'task\submitTask',
+	'task\task-pool',
+	'task\getTaskPoolList',
+	'task\freelancer-unpaid-task',
+		'task\getFreelancerAssigningTaskList',
+		
+	// -- Project
+	
 	'project\quote', // Quotes in menu
 	'project\detail', // Quotes in menu
 	'project\uploadFile',
@@ -359,7 +510,7 @@ return array(
 		'landing/file',
    		'file/wordcount',
     ),
-	'Freelancer'=> $allow_all,
+	'Freelancer'=> $allow_freelancer,
 	'Employer'=> $allow_employer,
     'Admin'=> $allow_all,
 );
