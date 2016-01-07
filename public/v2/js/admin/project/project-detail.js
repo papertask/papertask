db0fcdefbd0a8805b65756954b9fcb77b235c276
dinhthi1610 <todinhthi@gmail.com>
2016-01-08 00:03:17 +0700

    fix 299

--- public/v2/js/admin/project/project-detail.js
+++ public/v2/js/admin/project/project-detail.js
@@ -118,6 +118,10 @@ angularApp.controller('ProjectDetailController', function($scope, $rootScope, $h
 
 			$scope.project = $project;
 			$scope.currency = $scope.project.currency;
+			if($scope.currency == "2")
+				$scope.currency = "CNY";
+			console.log("$scope.currency");
+			console.log($scope.currency);
 			if($project.pm){
 			var ajaxPmUser = $http.get("/" + LANG_CODE + "/admin/staff/getUserByPm?staffid="+$project.pm.id)
             .success( function ( $data ) {
